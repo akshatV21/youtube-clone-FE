232a2df4f33cbe56ad5cffdee8904bf157c01eec
akshatV21 <a.vishwakarma2110@gmail.com>
2022-09-08 22:02:03 +0530

    added ifram

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from "../views/auth/LoginView.vue"
 import HomeView from "../views/home/HomeView.vue"
 import VideosView from "../views/home/VideosView.vue"
 import ChannelView from "../views/channel/ChannelView.vue"
+import WatchView from "../views/WatchView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +35,11 @@ const router = createRouter({
           component: VideosView,
         },
       ],
+    },
+    {
+      path: "/watch",
+      name: "watch",
+      component: WatchView,
     },
   ],
 })
---
+++ src/views/WatchView.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+
+<template>
+  <section class="bg">
+    <div class="video">
+      <div class="player">
+        <iframe src="https://www.youtube.com/embed/3_kobDkp_1g"></iframe>
+        <div class="info">
+          <div class="top">
+            <p class="title">This is the title of the video you want to watch!!</p>
+          </div>
+          <div class="desc"></div>
+          <div class="comments"></div>
+        </div>
+      </div>
+    </div>
+    <div class="videos"></div>
+  </section>
+</template>
+
+<style scoped>
+.bg {
+  width: 100%;
+  min-height: 100vh;
+  background-color: var(--not-very-dark-blue);
+}
+
+.video {
+  width: 75%;
+  min-height: 100vh;
+}
+
+iframe {
+  background-color: var(white-o-2);
+}
+</style>
